7f305cb491f1db510620251065e6fc72d40510e6
balddrAdmin <boliklevik@gmail.com>
2026-09-24 21:09:36 +0200

    db connect

---
+++ stadte_eintragen.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+
+aus_website = '''Аделаида	Adelaide	1 276 701	2014	Южная Австралия
+Брисбен	Brisbane	2143121	2013	Квинсленд
+Вуллонгонг	Wollongong	286581	2013	Новый Южный Уэльс
+Серферс-Парадайс	Gold Coast	605134	2013	Квинсленд
+Госфорд	Gosford	320266	2013	Новый Южный Уэльс
+Дарвин	Darwin	119597	2013	Северная территория
+Джелонг	Geelong	181853	2013	Виктория
+Канберра	Canberra	418856	2013	Австралийская столичная территория
+Кэрнс	Cairns	145003	2013	Квинсленд
+Мельбурн	Melbourne	4181021	2013	Виктория
+Ньюкасл	Newcastle	425895	2013	Новый Южный Уэльс
+Перт	Perth	1901582	2013	Западная Австралия
+Саншайн-Кост	Sunshine Coast	292354	2013	Квинсленд
+Сидней	Sydney	4373433	2013	Новый Южный Уэльс
+Таунсвилл	Townsville	176035	2013	Квинсленд
+Тувумба	Toowoomba	112588	2013	Квинсленд
+Хобарт	Hobart	206560	2013	Тасмания'''
+
+
+conn = psycopg2.connect(
+    database='stadte', user='admin', password='admin',
+    host='localhost', port='5432'
+)
+conn.autocommit = True
+cursor = conn.cursor()
+
+stadte_getrennt = aus_website.split('\n')
+for stadt in stadte_getrennt:
+    ...
