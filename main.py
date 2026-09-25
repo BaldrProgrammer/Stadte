6a8b520af839cc2ae690c44719aed5461a35a52f
balddrAdmin <boliklevik@gmail.com>
2026-09-25 17:58:47 +0200

    erster Version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import psycopg2
+import random
 
 conn = psycopg2.connect(
     database='stadte', user='admin', password='admin',
@@ -7,9 +8,21 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 lang = input('Wählen Sie bitte die Sprache des Spiels(ru/en): ')
+benutzerbuchstabe = ''
 stadte_waren = []
 
 while True:
     benutzers_stadt = input('- ')
-    sql = f'select * from stadte where {lang}_erste_buchstabe = {benutzers_stadt[-1].upper()};'
-    print(sql)
+    stadte_waren.append(benutzers_stadt)
+    sql = f'''select * from stadte where {lang}_erste_buchstabe = '{benutzers_stadt[-1].upper()}';'''
+    cursor.execute(sql)
+    passende_antworten = cursor.fetchall()
+
+    while True:
+        antwort = random.choice(passende_antworten)
+        if antwort[1] in stadte_waren:
+            continue
+        break
+
+    print('~', antwort[1])
+    stadte_waren.append(antwort[1])
